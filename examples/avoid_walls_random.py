0b10f7e5455ad2879d92fa00dcbbfeeb4fca6187
Matthias <memm74@users.noreply.github.com>
2025-02-14 12:13:15 +0100

    added avoid walls random

---
+++ examples/avoid_walls_random.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from trilobot import Trilobot, BUTTON_A
+import random
+
+"""
+Further demonstrating Trilobot's ultrasound distance sensor, this example will drive
+forward and then turn right to avoid obstacles it detects them with the sensor.
+
+Stop the example by pressing button A.
+"""
+print("Trilobot Example: Avoid Walls\n")
+
+
+SPEED = 0.7  # The speed to drive at
+TURN_DISTANCE = 30  # How close a wall needs to be, in cm, before we start turning
+
+tbot = Trilobot()
+
+# Start moving forward
+tbot.forward(SPEED)
+
+while not tbot.read_button(BUTTON_A):
+    distance = tbot.read_distance()
+
+    # Turn if we are too closer than the turn distance
+    if distance < TURN_DISTANCE:
+        if random.choice([True, False]): # Randomly turn left or right, shorter code than using a random element from a list e.g. direction = ['left', 'right'] 
+            tbot.turn_right(SPEED)
+        else:
+            tbot.turn_left(SPEED)
+    else:
+        tbot.forward(SPEED)
+    # No sleep is needed, as distance sensor provides sleep
